0485633bb87031ba4f9531b0c72011842b065b50
ReidoBoss <bryansagarino222@gmail.com>
2024-08-09 05:25:30 +0800

    test

--- futures/app/controllers/HomeController.scala
+++ futures/app/controllers/HomeController.scala
@@ -3,22 +3,18 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future}
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents)(implicit ec: ExecutionContext) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  val test = Future.successful(1)
+  val meow = Future.successful(2)
+  def index() = Action.async { implicit request: Request[AnyContent] =>
+    meow.map{
+      x=> 
+        Ok(x.toString)
+    }
   }
 }
+
